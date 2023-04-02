7349e7de006dffd60621337bb3376b01f1759bf5
connorryanbaker <connorbaker@gmail.com>
2023-04-02 15:48:02 -0400

    add player w/ basic movement

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -11,12 +11,16 @@ use prelude::*;
 
 struct State{
   map: Map,
+  player: Player,
 }
 
 impl State {
   fn new() -> Self {
     Self {
-      map: Map::new()
+      map: Map::new(),
+      player: Player::new(
+        Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+      ),
     }
   }
 }
@@ -24,7 +28,9 @@ impl State {
 impl GameState for State {
   fn tick(&mut self, ctx: &mut BTerm) {
     ctx.cls();
+    self.player.update(ctx, &self.map);
     self.map.render(ctx);
+    self.player.render(ctx);
   }
 }
 
